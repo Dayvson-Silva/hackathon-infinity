5982cd5867099e0124bb333f03ca1e89a218164a
Ryhan Nalbert <ryhannalbert@gmail.com>
2024-09-28 21:39:58 -0300

    kaka

--- kakaka/personagem.py
+++ kakaka/personagem.py
@@ -1,6 +1,8 @@
 def iniciar_game():
     import random
 
+    print("No ano 2000, com a virada do século, dois reinos gladiaram. o Reino do Sol, foi dominado pelo Reino das Sombras. Criaturas moradoras das regiões subcrustais organizaram investidas contra as obras da humanidade com objetivo de obter o poder do Planeta Terra. \n   O senhor da escuridão, junto com o seu exército de criaturas das sombras (DOFUS) querem promover o caos social, ecológico e em meio às guerras e à poluição,criar condições sub humanas de vida, dominando a Terra por completo com o objetivo de eliminar toda  a humanidade. \n   Os guardiões do planeta: humanos, mago e o elfo,têm o importante papel de reunirforças e voltar a conduzirem o destino de toda uma nação. Conhecer as artimanhas do mal e tornar-se cada vez mais forte é o único objetivo para combatê-los. Lado a lado os guardiões do Reino do Sol, representantes da justiça divina, tem como objetivo principal impedir o completo domínio do Reino das Sombras e garantir um planeta em condições mínimas para a sobrevivência dos humanos ainda existentes.")
+
     classes = int(input(f"Você tem 3 classes para escolher, estão entre eles o \n1 - Humano \n2 - Mago \n3 - Elfo \nEscolha qual classe você deseja ser: "))
     # attributes = []
 
